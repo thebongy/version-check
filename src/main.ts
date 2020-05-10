94c82d1156d200bb1d8628be84dc8e78cb595180
Rakshith Ravi <rakshith.ravi@gmx.com>
2020-05-10 18:32:13 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -74,8 +74,18 @@ async function run(): Promise<void> {
             core.info(`${version} is a new tag, all set to publish new release!`);
 		}
 		core.setOutput('rawVersion', rawVersion);
-        core.setOutput('versionChanged', changed.toString());
-        core.setOutput('releaseVersion', version);
+		core.setOutput('versionChanged', changed.toString());
+		core.setOutput('releaseVersion', version);
+		core.setOutput('versionParts', rawVersion.split('.'));
+		core.setOutput(
+			'releaseTags',
+			rawVersion
+				.split(".")
+				.map((_, i, versions) => {
+					return versions.slice(0, i).join(".");
+				})
+				.join(", ")
+		);
     } catch (error) {
         core.setFailed(error.message);
     }
