47ab9d10fd0757e09d54c1a963d5bfe5fc72e867
Rishit Bansal <rishit.bansal0@gmail.com>
2020-05-11 19:27:09 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -80,7 +80,7 @@ async function run(): Promise<void> {
             'releaseTags',
             rawVersion
                 .split('.')
-                .map((_, i, versions) => versions.slice(0, i).join('.'))
+                .map((_, i, versions) => versions.slice(0, i+1).join('.'))
                 .join(','),
         );
     } catch (error) {
